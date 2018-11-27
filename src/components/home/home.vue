01ffd26b88ce0c4007fefc1cd78743d7bf426355
乔治巴顿 <191186814.qq.com>
2018-11-27 10:56:42 +0800

    users-init

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -27,7 +27,7 @@
                         <i class="el-icon-menu"></i>
                         <span>用户管理</span>
                     </template>
-                    <el-menu-item index="user"><i class="el-icon-setting"></i>用户列表</el-menu-item>
+                    <el-menu-item index="users"><i class="el-icon-setting"></i>用户列表</el-menu-item>
                 </el-submenu>
                 <el-submenu index="2">
                     <template slot="title">
